fda3f3ffafabcebfe6619f9740cfb11e80e57ad8
Ilya Sulimenko <twid01gc@gmail.com>
2021-06-06 14:40:02 +0700

    delete useless files

--- lib/models/screen_arguments.dart
+++
@@ -1,7 +0,0 @@
-class ScreenArguments {
-  final int id;
-  final String status;
-  final String title;
-
-  ScreenArguments(this.status, [this.id, this.title]);
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:empty_project/main.dart';
+import 'package:i_note_mobile/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
